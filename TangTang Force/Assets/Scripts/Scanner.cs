6e666fa6ee9d7209fb6822870f9b1045f7c5ab7f
haribo <zhdl453@naver.com>
2024-02-11 15:07:35 +0900

    원거리공격

---
+++ TangTang Force/Assets/Scripts/Scanner.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scanner : MonoBehaviour
+{
+    public float scanRange;
+    public LayerMask targetLayer;
+    public RaycastHit2D[] targets; //결과물 담아줄 다수의변수
+    public Transform nearestTarget;
+
+    void FixedUpdate()
+    {
+        targets = Physics2D.CircleCastAll(transform.position, scanRange, Vector2.zero, 0, targetLayer);
+        nearestTarget = GetNearest();
+    }
+
+    Transform GetNearest() //가장 가까운것을 찾는 함수
+    {
+        Transform result = null;
+        float diff = 100;
+
+        foreach (RaycastHit2D target in targets)
+        {
+            Vector3 myPos = transform.position;
+            Vector3 targetPos = target.transform.position;
+            float curDiff = Vector3.Distance(myPos, targetPos);
+
+            if (curDiff < diff)
+            {
+                diff = curDiff; //반복문 돌며 가져온 거리가 저장된 거리보다 작으면 교체됨
+                result = target.transform;
+            }
+        }
+        return result;
+    }
+}
